ac0a0fd15397a25ccf009e9135a71d960106e4fe
michaelthezing <mtelezing23@gmail.com>
2023-03-08 21:07:05 -0800

    Create Account Page Rough Draft

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import './CreateAccount.css'
 const CreateAccount = () => {
     return(
-        <div>
-    <h1>Mail us on feedback@geeksforgeeks.org</h1>
-  </div>
+      
+      <body><div  className='Create'>
+        <img src = "https://content.sportslogos.net/logos/35/882/full/ucla_bruins_logo_mascot_20043890.png" className="bear" alt="logo"/>
+      <form>
+ <a>Create an Account</a>
+ <label for="username">Username:</label>
+ <input type="text" id="username" name="username" required></input>
+ <label for="password">Password:</label>
+ <input type="password" id="password" name="password" required></input>
+ <label for="email">Email:</label>
+ <input type="email" id="email" name="email" required></input>
+ <input type="submit" value="Create Account"></input>
+</form>
+   </div>  
+  </body>
+   
+  
+        
     );
     
 };
