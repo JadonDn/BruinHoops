d65578f4704a31214799bb613034f29ea6e173db
michaelthezing <mtelezing23@gmail.com>
2023-03-09 19:13:42 -0800

    Create Account, Login UI Changes

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import './CreateAccount.css'
 const CreateAccount = () => {
+
+
     return(
-      
-      <body><div  className='Create'>
-        <img src = "https://content.sportslogos.net/logos/35/882/full/ucla_bruins_logo_mascot_20043890.png" className="bear" alt="logo"/>
+      <div className='test'>
+        <body><div  className='Create'>
+        <img src = "mascot.png" className="bear" alt="logo"/>
       <form>
- <a>Create an Account</a>
+ <a>Create Account</a>
  <label for="username">Username:</label>
  <input type="text" id="username" name="username" required></input>
  <label for="password">Password:</label>
  <input type="password" id="password" name="password" required></input>
- <label for="email">Email:</label>
+ <label for="email">
+  
+ </label>
  <input type="email" id="email" name="email" required></input>
  <input type="submit" value="Create Account"></input>
 </form>
    </div>  
   </body>
+     </div>
    
   
         
