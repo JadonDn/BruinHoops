272848fc78516dbd4842fa2cf6706b915543f6da
michaelthezing <mtelezing23@gmail.com>
2023-03-09 19:14:08 -0800

    Create Account UI and Login UI changes

--- src/Login.js
+++ src/Login.js
@@ -11,22 +11,27 @@ function Login() {
           <div classname = "WebsiteName"><h2>Bruin</h2><h3>Hoops</h3></div>
           </p>
           <a>
-        <img src = "https://content.sportslogos.net/logos/35/882/full/ucla_bruins_logo_mascot_20043890.png" className="Login-logo" alt="logo"/>
+        <img src = "mascot.png" className="Login-logo" alt="logo"/>
   <body>
     <div class = "login-Section">
       <meta name ="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <h1>Login</h1>
+      <div className="Logincolor">
+
       <form>
         <label for ="username">Username</label>
         <input type = "text" id ="username" name = "password"></input>
         <label for="password">Password</label>
         <input type="password" id="password" name="password"></input>
         <input type="submit" value="Login"></input>
-        <Link to="/CreateAccount">
+        <Link to="/CreateAccount
+        ">
               Create Account 
             
         </Link>
       </form>
+      </div>
+  
     </div>
   </body>
           </a>
