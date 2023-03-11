7339cba851e627c50516943877a9611845494169
Aanas Chowdhury <aanaschowdhury@gmail.com>
2023-03-11 14:02:41 -0800

    Merge branches 'main' and 'main' of https://github.com/JadonDn/BruinHoops

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import logo from './logo.svg';
 import firebase from 'firebase/app';
 import './App.css'
+import {setMinutes, setHours} from "date-fns";
+import Swal from 'sweetalert2';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/app';
@@ -35,19 +40,14 @@ function App() {
   const localizer = momentLocalizer(moment);
 
   const [view, setView] = useState('week');
-  const [events, setEvents] = useState([
-    {
-      id: 1,
-      title: 'Meeting',
-      start: new Date(2023, 2, 5, 10, 0),
-      end: new Date(2023, 2, 5, 11, 0),
-    },
-  ]);
+  const [events, setEvents] = useState([]);
+
+  
 
   const eventStyleGetter = (event) => {
     return {
       style: {
-        backgroundColor: '#3174ad',
+        backgroundColor: 'red',
         color: '#fffff',
       },
     };
@@ -66,11 +66,71 @@ function App() {
   };
 
   const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+
+
 
   const toggleModal = () => {
+    if (modal) {
+      setTitle('');
+      setStartDate('');
+      setEndDate('');
 
-      setModal(!modal)
+    }
+  
+    setModal(!modal);
   };
+
+  const modalEventCheck = (start, end) => {
+
+    const { star } = start;
+    const {en} = end;
+    const canAdd = canAddEvent(start, end, events);
+    const isNotAllowed = moment(start).hour() < 8 || moment(end).hour() > 20; // set the time range here
+    const eventDuration = moment.duration(moment(end).diff(moment(start))).asHours(); // get the duration of the event in hours
+    const maxEventDuration = 2;
+    const minEventDuration = 0.5;
+
+    if (canAdd && !isNotAllowed && eventDuration <= maxEventDuration && eventDuration >= minEventDuration) {
+      return(true);
+    } else if (isNotAllowed && !modal) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Please select an available time slot',
+      });
+      setModal(false);
+      return(false);
+    } else if (eventDuration > maxEventDuration) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Courts may be reserved for up to 2 hours only.',
+      });
+      setModal(false);
+      return(false);
+    } else if (eventDuration < minEventDuration) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Courts must be reserved for at least 30 minutes',
+      });
+      setModal(false);
+      return(false);
+
+    } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'All courts are reserved at this time, please choose a different time.',
+          confirmButtonColor: 'rgb(120, 120, 255)'
+        });
+        setModal(false);
+        return(false);
+
+      }
+
+
+  }
 
   if(modal){
       document.body.classList.add('active-modl')
@@ -79,19 +139,9 @@ function App() {
   else{
       document.body.classList.remove('active-modl')
       document.body.style.overflow = 'auto';
+
   }
   
-  const handleClick = (eventTitle) => {
-    const newEvent = {
-      id: events.length + 1,
-      title: eventTitle,
-      start: new Date(),
-      end: new Date(),
-    };
-    setEvents([...events, newEvent]);
-    setInputValue(''); 
-  }; 
-
   const canAddEvent = (start, end, events) => {
     const maxEventsPerSlot = 4;
     const eventsDuringSlot = events.filter((event) => {
@@ -106,20 +156,44 @@ function App() {
   const handleSelectSlot = (slotInfo) => {
     const { start, end } = slotInfo;
     const canAdd = canAddEvent(start, end, events);
-    const isNotAllowed = moment(start).hour() < 8 || moment(end).hour() > 17; // set the time range here
-    if (canAdd && !isNotAllowed) {
-      const newEvent = {
-        id: events.length + 1,
-        title: 'MONKEY',
-        start: start,
-        end: end,
-      };
-      setEvents([...events, newEvent]);
-    } else if (isNotAllowed) {
-      alert('Please select an available time slot');
-    }
-    else {
-      alert('Maximum number of events per time slot reached');
+    const isNotAllowed = moment(start).hour() < 8 || moment(end).hour() > 20; // set the time range here
+    const eventDuration = moment.duration(moment(end).diff(moment(start))).asHours(); // get the duration of the event in hours
+    const maxEventDuration = 2;
+    const minEventDuration = 0.5;
+
+    if (canAdd && !isNotAllowed && eventDuration <= maxEventDuration && eventDuration >= minEventDuration) {
+      return(true);
+    } else if (isNotAllowed && !modal) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Please select an available time slot',
+      });
+      setModal(false);
+      return(false);
+    } else if (eventDuration > maxEventDuration) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Courts may be reserved for up to 2 hours only.',
+      });
+      setModal(false);
+      return(false);
+    } else if (eventDuration < minEventDuration) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Courts must be reserved for at least 30 minutes',
+      });
+      setModal(false);
+      return(false);
+
+    } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'All courts are reserved at this time, please choose a different time.',
+          confirmButtonColor: 'rgb(120, 120, 255)'
+        });
+        setModal(false);
+        return(false);
+
     }
   };
 
@@ -128,24 +202,35 @@ function App() {
     console.log('Selected Event:', event);
   };
 
-  const [inputValue, setInputValue] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(modalEventCheck(startDate, endDate))
+    {
+      const newEvent = {
+        id: events.length + 1,
+        title: title,
+        start: startDate,
+        end: endDate,
+      };
+      setEvents([...events, newEvent]);
+    }
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+  
+    toggleModal();
   };
 
   
 
-
   return (
     <div className="App">
-      <div className="header-container" style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center', paddingLeft: '20px'}}>
-        <h1 style={{marginBottom: '10px'}}>Events</h1>
+      <div className="header-container" >
+        <h1> Events</h1>
+      </div>
+      <div className="event-btn-container" >
+        <button onClick={toggleModal}className="btn-modal"> Reserve </button>
       </div>
       <div style={{clear: 'both'}}></div>
-      <div style={{paddingTop: '50px'}}>
-      <button onClick={toggleModal}className="btn-modal"> Open </button>
-
+      <div style={{paddingTop: '1px'}}>
         <Calendar
           localizer={localizer}
           events={events}
@@ -163,7 +248,11 @@ function App() {
           min={new Date(0, 0, 0, 8, 0)} // start time of day view
           max={new Date(0, 0, 0, 20, 0)} // end time of day view
           selectable={true}
-          onSelectSlot={(slotInfo) => handleSelectSlot(slotInfo)}
+          onSelectSlot={(slotInfo) => { 
+            setModal(handleSelectSlot(slotInfo));  
+            setStartDate(slotInfo.start);
+            setEndDate(slotInfo.end);
+            ;}}
           onSelectEvent={handleSelectEvent}
         />
               <div className="button-container" style={{marginLeft: '20px'}}>
@@ -174,14 +263,45 @@ function App() {
             onClick={toggleModal}
             className="overlay"></div>
             <div className="modal-content">
-                <h2> Hello Modal</h2>
-                <p>
-                    lorum psum
-                </p>
-                <button
-                className='close-modal'
-                onClick={toggleModal}
-                >CLOSE</button>
+              <h2>Reserve a Court!</h2>
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title:</label> 
+                <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+              <div className="input-group">
+                <label htmlFor="start">Start:</label>
+                <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                showTimeSelect
+                timeFormat="h:mm aa"
+                timeIntervals={30}
+                timeCaption="time"
+                minTime={new Date().setHours(8, 0)}
+                maxTime={new Date().setHours(20, 0)}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                className="custom-date-picker"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="end">End:</label>
+                <DatePicker
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                disabled={!startDate}
+                showTimeSelect
+                timeFormat="h:mm aa"
+                timeIntervals={30}
+                timeCaption="time"
+                minDate={startDate}
+                minTime={setMinutes(startDate, 30)}
+                maxTime={new Date().setHours(20, 0)}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                className="custom-date-picker"
+                />
+              </div>
+                <button type="submit">Create Event</button>
+              </form>
+            <button className='close-modal' onClick={toggleModal}>Cancel</button>
             </div>
          </div>)}
         </>
