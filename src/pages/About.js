f9149f60b43c7a328d80668270ea7a5af3cb2a72
michaelthezing <mtelezing23@gmail.com>
2023-03-12 22:30:00 -0700

    Contact UI + User Can Email Us From Website

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,13 +5,13 @@ import NavBar from "../components/NavBar";
 const About = () => {
     return(
       <div className="About">
-        <NavBar />
+        <NavBar/>
         <img src = "https://www.freeiconspng.com/thumbs/calendar-image-png/calendar-image-png-3.png" className='cal'></img>
         <p>Bruin Hoops allows students at UCLA to schedule games and check the availability of various basketball courts around campus. 
           This includes Hitch courts and the John Wooden Center. Based on the John Wooden Center and Hitch courts’ hours of operation, students 
           are able to create events, team up with others, and reserve courts to host fun and competitive basketball games!</p>
           <h1>Our Approach</h1>
-          
+       
       </div>
     );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,10 +4,38 @@ import NavBar from "../components/NavBar";
 
 function Contact() {
     return(
-        <div className = "Contact">
-            <NavBar />
-            <p>BruinHoops@gmail.com</p>
-        </div>
+
+   
+        <body>
+             <NavBar/>
+             
+            <div class="Contact">
+        <h1>Have any questions?</h1>
+        <p>For any inquiries please contact us through email!</p>
+        <form target="_blank" action="https://formsubmit.co/mtelezing23@gmail.com" method="POST">
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col">
+                <input type="text" name="name" class="form-control" placeholder="Full Name" required></input>
+              </div>
+              <div class="col">
+                <input type="email" name="email" class="form-control" placeholder="Email Address" required></input>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+          </div>
+          <button type="Sub" class="btn btn-lg btn-dark btn-block">Submit Email</button>
+    
+        </form>
+        
+      </div>
+      
+        </body>
+            
+                   
+       
     );
 }
 
