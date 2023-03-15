d3523dab0e2ec97270bd58411a6aa3e9a7be0d78
Marco Rovell <mrovell@ucla.edu>
2023-03-15 16:55:47 -0700

    bug fixes for edio bio

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,134 @@
+// import './Profile.css';
+// import NavBar from "../components/NavBar";
+// import { auth, db } from '../firebase';
+// import { doc, getDoc } from "firebase/firestore";
+// import { useState, useEffect } from 'react';
+// import Swal from 'sweetalert2';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import { Calendar, momentLocalizer } from 'react-big-calendar';
+// import moment from 'moment';
+
+// const Profile = () => {
+
+
+//     const editProfile = () => {
+
+//         const [Bio, setBio] = useState('')
+    
+//         Swal.fire({
+//             title: 'Edit Bio',
+//             input: "text",
+//             showCancelButton: true,
+//             closeOnConfirm: false,
+//             confirmButtonColor: '#007bff',
+//             confirmButtonText: "Submit",
+//             cancelButtonText: 'Cancel',
+//             inputPlaceholder: "Write something about yourself...",
+//             preConfirm: (inputValue) => {
+//                 if (!inputValue.trim()) {
+//                   return Promise.reject('You must enter some input!');
+//                 }
+//               }
+//         }).then((result) => {
+//             if (result.isConfirmed) {
+//                 setBio(result.value);
+//             } else {
+    
+//             }
+//           });
+//     }
+
+
+// const localizer = momentLocalizer(moment);
+
+// const [view, setView] = useState('week');
+// const [events, setEvents] = useState([]);  
+
+// const eventStyleGetter = (event) => {
+//     return {
+//       style: {
+//         backgroundColor: '#007bff',
+//         color: '#fffff',
+//       },
+//     };
+//   };
+
+//   const dayFormat = (date, culture, localizer) => {
+//     return localizer.format(date, 'dd');
+//   };
+
+//   const timeFormat = (date, culture, localizer) => {
+//     return localizer.format(date, 'h:mm a');
+//   };
+
+//   const headerFormat = (date, culture, localizer) => {
+//     return localizer.format(date, 'MMMM YYYY');
+//   };
+
+//     const [userData, setuserData] = useState(null);
+
+//     useEffect(() => {
+//         const fetchUserData = async () => {
+//           const user = auth.currentUser;
+//         const docRef = doc(db, "users", user.uid);
+//         const docSnap = await getDoc(docRef);
+//         if (docSnap.exists()) {
+//             setuserData(docSnap.data());
+//         } else {
+//             console.log("No such document!");
+//         }
+//     };    
+//     fetchUserData();
+//     }, []);
+
+//     return (
+//         <div>
+//             <NavBar />
+//             <div className="profile">
+//                 <div className="profile-container">
+//                     <h2 className="profile-header">Your Profile</h2>
+//                     <div className="profile-content">
+//                         <img src = "pfp_placeholder.png" className='profile-image'></img>
+//                         <h3 className="profile-name">Username: {userData?.username}</h3>
+//                         <h4 className="profile-email">Email: {userData?.email}</h4>
+//                         <p className="profile-bio">"insert bio"</p>
+//                         <button onClick={() => {editProfile()}} className="">Edit Bio</button>
+                       
+                     
+
+//                     </div>
+//                 </div>
+//             </div>
+           
+//             <div className='cal'> <Calendar  localizer={localizer}
+//           events={events}
+//           startAccessor="start"
+//           endAccessor="end"
+//           views={['week', 'day']}
+//           min={new Date(0, 0, 0, 8, 0)} // start time of day view
+//           max={new Date(0, 0, 0, 20, 0)} // end time of day view
+//           defaultView={'week'}
+//           eventPropGetter={eventStyleGetter}
+//           dayFormat={dayFormat}
+//           timeFormat={timeFormat}
+//           headerFormat={headerFormat}
+//           style={{ height: 475, width: 725, paddingLeft: '50px', paddingRight: '50px', }}
+//           intervalHeight={50}
+//           slotDuration={'00:30:00'}
+
+//         /> </div> 
+//         </div>
+//     );
+// }
+
+// export default Profile;
+
+
 import './Profile.css';
 import NavBar from "../components/NavBar";
 import { auth, db } from '../firebase';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { onAuthStateChanged} from 'firebase/auth';
 import { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -39,17 +166,19 @@ const eventStyleGetter = (event) => {
     const [userData, setuserData] = useState(null);
 
     useEffect(() => {
-        const fetchUserData = async () => {
-          const user = auth.currentUser;
-        const docRef = doc(db, "users", user.uid);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            setuserData(docSnap.data());
-        } else {
-            console.log("No such document!");
-        }
-    };    
+        onAuthStateChanged(auth, (user) => {
+            const fetchUserData = async () => {
+              const user = auth.currentUser;
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setuserData(docSnap.data());
+            } else {
+                console.log("No such document!");
+            }
+        };
     fetchUserData();
+        })
     }, []);
 
     return (
@@ -59,14 +188,11 @@ const eventStyleGetter = (event) => {
                 <div className="profile-container">
                     <h2 className="profile-header">Your Profile</h2>
                     <div className="profile-content">
-                        <img src = "pfp_placeholder.png" className='profile-image'></img>
+                        <img src = {userData?.profile_photo}className='profile-image' alt="profile_photo"></img>
                         <h3 className="profile-name">Username: {userData?.username}</h3>
                         <h4 className="profile-email">Email: {userData?.email}</h4>
-                        <p className="profile-bio">"insert bio"</p>
+                        <p className="profile-bio">Bio: {userData?.bio}</p>
                         <button onClick={editProfile}className="">Edit Bio</button>
-                       
-                     
-
                     </div>
                 </div>
             </div>
@@ -93,11 +219,27 @@ const eventStyleGetter = (event) => {
 }
 
 const editProfile = () => {
+    const uid = auth.currentUser.uid
+    const userRef = doc(db, "users", uid)
     Swal.fire({
-        icon: 'error',
-        title: 'CHANGE ME!',
-        confirmButtonColor: '#007bff',
-    });
+      title: 'Write a bio',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Confirm',
+      showLoaderOnConfirm: true,
+      preConfirm: (inputValue) => {
+        if (!inputValue.trim()) {
+            Swal.showValidationMessage('You Must Write Something!');        }
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+        if (result.isConfirmed) {
+            updateDoc(userRef, { bio : result.value }, { merge : true}, {})
+        }
+    })
 }
 
 export default Profile;
