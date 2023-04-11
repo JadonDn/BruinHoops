dff8c44641ca9da95942f199a5ab72da2d2a483d
Marco Rovell <114272849+MarcoRovell@users.noreply.github.com>
2023-04-11 15:58:30 -0700

    Update firebase.js
    removed API keys, project no longer in use. To run this project, use your own API keys.

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,6 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDM50IoA5Ah4Na_h5fyHBIHDPrONjtZ9jk",
-  authDomain: "bruin-hoops.firebaseapp.com",
-  databaseURL: "https://bruin-hoops-default-rtdb.firebaseio.com",
-  projectId: "bruin-hoops",
-  storageBucket: "bruin-hoops.appspot.com",
-  messagingSenderId: "133820248934",
-  appId: "1:133820248934:web:20b9bb7e3729c3f2668ea7",
-  measurementId: "G-JNL13FVSXB"
 };
 
 export const app = initializeApp(firebaseConfig);
